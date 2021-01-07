73d5b6c8f1955384f45f688d115aa93e23cec24d
tikcuf2 <jared.martin@bryanuniversity.edu>
2021-01-07 03:07:39 -0800

    joke inside, sorry its late

---
+++ week1/selecting_elements/index.js
@@ -0,0 +1,3 @@
+var blank = document.getElementById ("blank").textContent += " Riddle : What kind of car does and egg drive ? ";
+
+var answer = document.getElementById("answer").textContent += "A YOKES-WAGGON ! ! !";
