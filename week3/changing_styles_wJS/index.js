55f57994249ed2f71247582d5c8bb7a702481597
tikcuf2 <jared.martin@bryanuniversity.edu>
2021-01-28 16:05:37 -0800

    changes to week 3.... Nailed it !

--- week3/changing_styles_wJS/index.js
+++ week3/changing_styles_wJS/index.js
@@ -1,15 +1,12 @@
 for (var i = 0; i < 5 ; i++){
-    var h1 = document.createElement("h1");
+    var al = document.createElement("h1");
 
-    h1.textContent = "HelloWorld";  //inline java style?//
-    document.body.append(h1);
-    document.getElementsByTagName(h1);
-    h1.style.fontSize = ("20px");
-    h1.style.fontWeight = ("lighter");
-    h1.style.fontFamily = ("sans-serif");
-    h1.style.color = ("cornflowerblue");
-  
+    al.textContent = "HelloWorld"; 
+    al.style.fontSize = "20px";
+    al.style.fontWeight = "lighter";
+    al.style.fontFamily = "sans-serif";
+    al.style.color = "cornflowerblue";
+    al.classList.add("border");
+    document.body.appendChild(al);
 }
-    var classList = getComputedStyle(".border");
-
     
