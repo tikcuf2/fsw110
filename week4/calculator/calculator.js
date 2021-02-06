59076050bb67aed2cfec00ebf75c45e8b9f76fa8
tikuf2 <jared.martin@bryanuniversity.edu>
2021-02-06 00:39:00 -0800

    calculator

--- week4/calculator/calculator.js
+++ week4/calculator/calculator.js
@@ -1,13 +1,50 @@
 
 
-display     // sum
+
+
+const buttons = document.querySelectorAll('#submit');
+console.log (buttons)
+buttons.forEach(function(button) {
+  console.log (button)
+   button.addEventListener('click', calculate);
+ });
+
+// calculate function
+function calculate(event) {
+  console.log(event.target)
+   // current clicked buttons value
+   const clickedButtonValue = event.target.innerHTML;
+   
+   console.log(clickedButtonValue,clickedButtonValue.length)
+   
+   if (clickedButtonValue === '+') {
+    console.log('adding')
+       var add1 = document.getElementById ("add1").value 
+       var add2 = document.getElementById ("add2").value
+       console.log(add1 + add2)
+       var num1 = Number(add1)
+       var num2 = Number(add2)
+       console.log(num1 + num2)
+       display.innerHTML = eval(num1+num2);
+       console.log(display)
+     }
+   
+   
+   else  display.innerHTML += (clickedButtonValue);
+ }
 
 
 
+
+
+
+const display = document.querySelector('#display');
+console.log(display)
 
 
    // addition
-Calculate(MyFirstNumber, MySecondNumber, Event)
+// document.getElementById(box1)//
+// var addition = (first (Number)) + (second(Number))
 
 
 
